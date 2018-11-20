cc304ef3fd09555e339ed5f5884b8238535c6afc
surapoom <45107078+surapoom@users.noreply.github.com>
2018-11-20 11:08:52 +0700

    Change to my local

--- _PullFromMT4.bat
+++ _PullFromMT4.bat
@@ -3,7 +3,7 @@ rem Script to Sync Files from Development Terminal to Version Control
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
-set DEST_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\Include"
+set SOURCE_DIR="C:\Program Files (x86)\ICMarkets MT4 Terminal2\MQL4\Include"
+set DEST_DIR="C:\LazyTrading\GitHub\Include"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mqh
