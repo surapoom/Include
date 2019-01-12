b26f975f1be0801e40548b380132baec0a894bb8
surapoom <45107078+surapoom@users.noreply.github.com>
2019-01-13 00:45:35 +0700

    Adapt to local

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\Include"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Include"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Include"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Include"
+set SOURCE_DIR="C:\LazyTrading\GitHub\Include"
+set DEST_DIR1="C:\Program Files (x86)\ICMarkets MT4 Terminal1\MQL4\Include"
+set DEST_DIR2="C:\Program Files (x86)\ICMarkets MT4 Terminal2\MQL4\Include"
+set DEST_DIR3="C:\Program Files (x86)\ICMarkets MT4 Terminal3\MQL4\Include"
+set DEST_DIR4="C:\Program Files (x86)\ICMarkets MT4 Terminal4\MQL4\Include"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mqh
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mqh
